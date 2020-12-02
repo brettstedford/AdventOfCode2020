5524252dd1acb5859402cb49ea13098f6ca4d5b4
Brett Stedford <brett.stedford@ao.com>
2020-12-02 15:15:52 +0000

    Initial commit with final implementations for challenges 1 and 2

---
+++ Challenge2/PasswordPolicy.cs
@@ -0,0 +1,46 @@
+﻿namespace Challenge2
+{
+    public class PasswordPolicy
+    {
+        public string Password { get; }
+        public Policy Policy { get; }
+
+        public PasswordPolicy(string passwordPolicyAsString)
+        {
+            // regex if you want to be fancy
+            var bits = passwordPolicyAsString.Split(":");
+            Policy = new Policy(bits[0].Trim());
+            Password = bits[1].Trim();
+        }
+
+        public bool Valid()
+        {
+            var chars = Password.ToCharArray();
+
+            var lowestIndex = Policy.MinimumExpected - 1;
+            var highestIndex = Policy.MaximumAllowed - 1;
+            
+            return (chars[lowestIndex] == Policy.ExpectedCharacter ||
+                chars[highestIndex] == Policy.ExpectedCharacter) && 
+                   !(chars[lowestIndex] == Policy.ExpectedCharacter &&
+                     chars[highestIndex] == Policy.ExpectedCharacter);
+        }
+    }
+
+    public class Policy
+    {
+        public int MinimumExpected { get; }
+        public int MaximumAllowed { get; }
+        public char ExpectedCharacter { get; }
+        public string Display { get;  }
+        
+        public Policy(string policyAsString)
+        {
+            Display = policyAsString;
+            var bits = policyAsString.Split(" ");
+            MinimumExpected = int.Parse(bits[0].Split("-")[0]);
+            MaximumAllowed = int.Parse(bits[0].Split("-")[1]);
+            ExpectedCharacter = char.Parse(bits[1].Trim());
+        }
+    }
+}
---
+++ Challenge2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Challenge2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int validPasswords = 0;
+            
+            foreach (var ppas in Input.RealPasswordPoliciesAsStrings)
+            {
+                var pp = new PasswordPolicy(ppas);
+
+                var result = "INVALID";
+
+                if (pp.Valid())
+                {
+                    result = "VALID";
+                    validPasswords++;
+                }
+
+                Console.WriteLine($"{pp.Password} - {pp.Policy.Display} - {result}");
+            }
+            
+            Console.WriteLine($"# Valid Password: {validPasswords}");
+        }
+    }
+}
